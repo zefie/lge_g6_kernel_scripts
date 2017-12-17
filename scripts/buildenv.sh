ddae524a1fc38c83b69692c3412f8fc8c5d07c33
zefie <zefie@zefie.net>
2017-12-17 04:26:04 -0500

    zefie toolchain layout stuff

--- scripts/buildenv.sh
+++ scripts/buildenv.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
-export PATH="${PWD}/.zefie/lz4demo:${PATH}"
-export TOOLCHAIN="/home/zefie/dev/toolchains/uber/out/aarch64-linux-android-6.x/bin/aarch64-linux-android-"
+# Update this and uncomment it
 
-export KERNEL_MANU="LG"
-export KERNEL_MODEL="G6"
-export KERNEL_DEVMODEL="US997"
-export KERNEL_DEFCONFIG="lucye_nao_us-perf_defconfig"
+#export TOOLCHAIN="/home/zefie/dev/toolchains/uber/out/aarch64-linux-android-6.x/bin/aarch64-linux-android-"
+export KERNEL_NAME="melina" # please change from melina for custom builds
+export KERNEL_DEVNAME="${USER}" # can be normal name, defaults to linux username ;)
 
-export KERNEL_DEVNAME="$(whoami)" # can be normal name, defaults to linux username ;)
-export KERNEL_NAME="melina"
+export KERNEL_MANU="LG" # for zip filename
+export KERNEL_MODEL="G6" # for zip filename
+export KERNEL_DEVMODEL="US997" # for zip filename, and anykernel whitelist (converted to lowercase for whitelist)
+export KERNEL_DEFCONFIG="lucye_nao_us-perf_defconfig" # device, check your build fingerprint
 
-export ANDROID_TARGET="LGE-Stock"
-export KERNEL_BUILDDIR="build"
+export ANDROID_TARGET="STOCK" # Could be Lineage-14.0, or whatever. Just for zip name.
+export KERNEL_BUILDDIR="build" # A subdirectory in this repo that is in .gitignore and doesn't already exist. Best just leave it be.
 
+# Zefie Toolchain Layout
+KERNEL_ZEFIE_TOOLCHAIN_LAYOUT=1 # You probably want this as 0
+ZEFIE_TC_NAME="uber"
+ZEFIE_TC_TYPE="aarch64-linux-android"
+ZEFIE_TC_VER="6.x"
 # Do not edit below this line
 
+export PATH="${PWD}/.zefie/lz4demo:${PATH}"
 export ARCH="arm64"
-export CROSS_COMPILE="${TOOLCHAIN}"
 export KERNEL_COMPRESSION_SUFFIX="lz4"
+
+# Allow env override
+if [ -z "${TC_NAME}" ]; then export TC_NAME="${ZEFIE_TC_NAME}"; fi
+if [ -z "${TC_TYPE}" ]; then export TC_TYPE="${ZEFIE_TC_TYPE}"; fi
+if [ -z "${TC_VER}" ]; then export TC_VER="${ZEFIE_TC_VER}"; fi
+
+if [ ${KERNEL_ZEFIE_TOOLCHAIN_LAYOUT} -eq 1 ]; then
+	# Example toolchain path: /home/zefie/dev/toolchains/available/uber_aarch64-linux-android-6.x
+	TC_ROOT="/home/${USER}/dev/toolchains/available"
+	export TOOLCHAIN="${TC_ROOT}/${TC_NAME}_${TC_TYPE}-${TC_VER}/bin/${TC_TYPE}-"
+fi
+
+export CROSS_COMPILE="${TOOLCHAIN}"
 
 if [ "$(basename $0)" == "buildenv.sh" ]; then
 	if [ ! -z "$1" ]; then
--- scripts/buildzip.sh
+++ scripts/buildzip.sh
@@ -4,6 +4,7 @@ KERNDIR="$(pwd)"
 OUTDIR="${KERNDIR}/build/out"
 TMPDIR="${OUTDIR}/build"
 MODDIR="${TMPDIR}/modules"
+LOGFIL="${OUTDIR}/buildzip.log"
 MODULES=1
 
 KERNEL_IMAGE="build/arch/${ARCH}/boot/Image.${KERNEL_COMPRESSION_SUFFIX}-dtb"
@@ -54,18 +55,20 @@ INCLUDED
 fi
 
 mkdir -p "${OUTDIR}"
-echo "*** Starting buildzip: $(date)" >> "${OUTDIR}/buildzip.log"
+echo "*** Starting buildzip: $(date)" >> "${LOGFIL}"
 echo "*** Starting buildzip: $(date)"
-echo "*** Kernel Name: ${KERNEL_NAME}" >> "${OUTDIR}/buildzip.log"
-echo "*** Kernel Device: ${KERNEL_MANU} ${KERNEL_MODEL} (${KERNEL_DEVMODEL})" >> "${OUTDIR}/buildzip.log"
-echo "*** Kernel Version: ${KVER}" >> "${OUTDIR}/buildzip.log"
+echo "*** Kernel Name: ${KERNEL_NAME}" >> "${LOGFIL}"
+echo "*** Kernel Device: ${KERNEL_MANU} ${KERNEL_MODEL} (${KERNEL_DEVMODEL})" >> "${LOGFIL}"
+echo "*** Kernel Version: ${KVER}" >> "${LOGFIL}"
 
 rm -rf "${TMPDIR}"
 mkdir -p "${TMPDIR}"
 echo " * Copying template to build directory"
-cp -rfv .zefie/scripts/recovery_zip_template/* "${TMPDIR}" >> "${OUTDIR}/buildzip.log"
+cp -rfv .zefie/scripts/recovery_zip_template/* "${TMPDIR}" >> "${LOGFIL}"
 find "${TMPDIR}" -name "placeholder" -exec rm {} +
 echo " * Patching template ..."
+MODEL_WHITELIST=$(echo "${KERNEL_DEVMODEL}" | tr '[:upper:]' '[:lower:]');
+sed -i -e 's/\%MODEL_WHITELIST\%/'"${MODEL_WHITELIST}"'/' "${TMPDIR}/anykernel.sh"
 sed -i -e 's/\%KERNELDEV\%/'"${KERNEL_DEVNAME}"'/' "${TMPDIR}/anykernel.sh"
 sed -i -e 's/\%NAME\%/'"${KERNEL_NAME}"'/' "${TMPDIR}/anykernel.sh"
 sed -i -e 's/\%MANU\%/'"${KERNEL_MANU}"'/' "${TMPDIR}/anykernel.sh"
@@ -77,14 +80,14 @@ sed -i -e 's/\%TOOLCHAIN_VERSION\%/'"${TCVER}"'/' "${TMPDIR}/anykernel.sh"
 if [ ${MODULES} -eq 1 ]; then
 	rm -rf "${TMPDIR}/_modtmp"
 	mkdir -p "${TMPDIR}/_modtmp"
-	.zefie/scripts/make.sh INSTALL_MOD_PATH="${TMPDIR}/_modtmp" modules_install 2>&1 >> "${OUTDIR}/buildzip.log"
+	.zefie/scripts/make.sh INSTALL_MOD_PATH="${TMPDIR}/_modtmp" modules_install 2>&1 >> "${LOGFIL}"
 
         # Rename exfat module for compatiblity (LG uses propritary Tuxera, we use open source)
 	for m in ${INCLUDED_MODULES}; do
 		if [ ! -z $(echo ${m} | grep '=') ]; then
 			mout=$(echo "${m}" | cut -d'=' -f1)
 			min=$(echo "${m}" | cut -d'=' -f2)
-			echo " -*-  Found ${min}, placing as ${mout} ..." >> "${OUTDIR}/buildzip.log"
+			echo " -*-  Found ${min}, placing as ${mout} ..." >> "${LOGFIL}"
 		else
 			min="${m}"
 			mout="${m}"
@@ -108,9 +111,15 @@ fi
 
 cp "${KERNEL_IMAGE}" "${TMPDIR}/zImage"
 #echo " * Generating QCDT..."
-#build/scripts/dtbTool/dtbTool -o "${TMPDIR}/dtb" -d build/scripts/dtc/dtc build/arch/arm64/boot/dts/ 2>&1 >> "${OUTDIR}/buildzip.log"
+#build/scripts/dtbTool/dtbTool -o "${TMPDIR}/dtb" -d build/scripts/dtc/dtc build/arch/arm64/boot/dts/ 2>&1 >> "${LOGFIL}"
 
-OUTFILE="boot_${KERNEL_MANU}-${KERNEL_DEVMODEL}_${KVER}_$(date --utc +%Y.%m.%d)_${ANDROID_TARGET}_${KERNEL_DEVNAME}.zip"
+if [ ! -z "${TC_VER}" ]; then
+	# zefie's layout enhanced naming
+	OUTFILE="boot_${KERNEL_MANU}-${KERNEL_DEVMODEL}_${KVER}_$(date --utc +%Y.%m.%d)_${TC_NAME}-${TC_VER}_${ANDROID_TARGET}_${KERNEL_DEVNAME}.zip"
+else
+	# Standard naming
+	OUTFILE="boot_${KERNEL_MANU}-${KERNEL_DEVMODEL}_${KVER}_$(date --utc +%Y.%m.%d)_${ANDROID_TARGET}_${KERNEL_DEVNAME}.zip"
+fi
 
 if [ -f "${OUTDIR}/${OUTFILE}" ]; then
 	rm -f "${OUTDIR}/${OUTFILE}"
@@ -118,13 +127,18 @@ fi
 
 cd "${TMPDIR}"
 echo " * Creating zip..."
-zip -r9y "${OUTDIR}/${OUTFILE}" . 2>&1 >> "${OUTDIR}/buildzip.log"
+zip -r9y "${OUTDIR}/${OUTFILE}" . 2>&1 >> "${LOGFIL}"
 cd "${KERNDIR}"
 echo ""
-echo "Flashable zip available at:"
-echo "${OUTDIR}/${OUTFILE}"
-echo ""
-echo "Generated ${OUTDIR}/${OUTFILE}" >> "${OUTDIR}/buildzip.log"
-echo "*** Ending buildzip: $(date)" >> "${OUTDIR}/buildzip.log"
-echo "" >> "${OUTDIR}/buildzip.log"
+if [ -f "${OUTDIR}/${OUTFILE}" ]; then
+	echo "Flashable zip available at:"
+	echo "${OUTDIR}/${OUTFILE}"
+	echo ""
+	echo "Generated ${OUTDIR}/${OUTFILE}" >> "${LOGFIL}"
+else
+	echo "Could not create flashable zip. See above for possible error messages."
+	echo "Error creating zip..." >> "${LOGFIL}"
+fi
+echo "*** Ending buildzip: $(date)" >> "${LOGFIL}"
+echo "" >> "${LOGFIL}"
 echo "*** Ending buildzip: $(date)"
