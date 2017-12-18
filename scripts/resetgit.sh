e5f53fcb1ef85deec89d5e4f4fc332ed4e647dee
zefie <zefie@zefie.net>
2017-12-18 06:55:04 -0500

    rewrite scripts to support mounted build dir

--- scripts/resetgit.sh
+++ scripts/resetgit.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+source .zefie/scripts/buildenv.sh
+
 if [ -d ".git" ]; then
 	if git diff-index --name-only HEAD | grep -qv "^scripts/package"; then
 		if git diff-index --name-only HEAD | grep -qv "^.zefie"; then
 			echo "Resetting source tree..."
-			rm * .config .config.old -rf
+			for f in $(ls . | grep -v "^${KERNEL_BUILDDIR}$"); do
+				rm -rf "${f}"
+			done;
+			rm -f .config .config.old
+			rm -rf "${KERNEL_BUILDDIR}/"*
 			git reset --hard
 		fi
 	fi
