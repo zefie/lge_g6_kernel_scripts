869919c5a7cf9eca5fa21d5447ff8453946a13f9
zefie <zefie@zefie.net>
2017-12-19 13:06:58 -0500

    resetgit.sh: add force reset option

--- scripts/resetgit.sh
+++ scripts/resetgit.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 source .zefie/scripts/buildenv.sh
 
+function z_reset_git {
+	echo "Resetting source tree..."
+	for f in $(ls . | grep -v "^${KERNEL_BUILDDIR}$"); do
+		rm -rf "${f}"
+	done;
+	rm -f .config .config.old
+	rm -rf "${KERNEL_BUILDDIR}/"*
+	git reset --hard
+}
+
 if [ -d ".git" ]; then
+	if [ ! -z "${FORCE_RESET}" ]; then
+		z_reset_git
+		exit
+	fi
+
 	if git diff-index --name-only HEAD | grep -qv "^scripts/package"; then
 		if git diff-index --name-only HEAD | grep -qv "^.zefie"; then
-			echo "Resetting source tree..."
-			for f in $(ls . | grep -v "^${KERNEL_BUILDDIR}$"); do
-				rm -rf "${f}"
-			done;
-			rm -f .config .config.old
-			rm -rf "${KERNEL_BUILDDIR}/"*
-			git reset --hard
+			z_reset_git
 		fi
 	fi
 fi
